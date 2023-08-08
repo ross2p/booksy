1bb3f28469b5ce971a80306d0c7dae9686c34019
Lev <levpredko@gmail.com>
2023-08-08 16:59:11 +0300

    added another catch

--- src/Salon/Main.java
+++ src/Salon/Main.java
@@ -1,7 +1,9 @@
 package Salon;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -11,17 +13,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Gson gson = new Gson();
         List<Salon> salons = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader("src/Salon/salon.json"))) {
-            Type type = new TypeToken<List<Salon>>(){}.getType();
+            Type type = new TypeToken<List<Salon>>() {
+            }.getType();
             salons = gson.fromJson(reader, type);
-            for(Salon salon : salons){
+            for (Salon salon : salons) {
                 System.out.println(salon);
             }
-        } catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.err.println("File not found.");
+        } catch (JsonSyntaxException e) {
+            System.err.println("Invalid JSON syntax in the file.");
         } catch (IOException e) {
             e.printStackTrace();
         }
