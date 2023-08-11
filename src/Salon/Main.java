9769fed87ab3763cb6b1f3590f524bbef5f5b7d4
Lev <levpredko@gmail.com>
2023-08-11 18:09:10 +0300

    added the time and appearance

--- src/Salon/Main.java
+++ src/Salon/Main.java
@@ -1,18 +1,5 @@
 package Salon;
 
-import Salon.Class.Salon;
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
-import com.google.gson.reflect.TypeToken;
-
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
--- src/Salon/Start.java
+++ src/Salon/Start.java
@@ -2,23 +2,22 @@ package Salon;
 
 import java.util.Scanner;
 
+
 public class Start {
+    public static Scanner sc = new Scanner(System.in);
     public static void start(){
         String start = "\u001B[34m"+"""
-                 .oooooo..o           oooo                       \s
-                d8P'    `Y8           `888                       \s
-                Y88bo.       .oooo.    888   .ooooo.  ooo. .oo.  \s
-                 `"Y8888o.  `P  )88b   888  d88' `88b `888P"Y88b \s
-                     `"Y88b  .oP"888   888  888   888  888   888 \s
-                oo     .d8P d8(  888   888  888   888  888   888 \s
-                8""88888P'  `Y888""8o o888o `Y8bod8P' o888o o888o\s
-                                                                 \s
-                Ross&Lev company                                 \s
-                                                                 \s
+                Search engine for salons                                               \s
+                                                                                       \s
+                ██████╗  ██████╗  ██████╗ ██╗  ██╗███████╗██╗   ██╗                    \s
+                ██╔══██╗██╔═══██╗██╔═══██╗██║ ██╔╝██╔════╝╚██╗ ██╔╝                    \s
+                ██████╔╝██║   ██║██║   ██║█████╔╝ ███████╗ ╚████╔╝                     \s
+                ██╔══██╗██║   ██║██║   ██║██╔═██╗ ╚════██║  ╚██╔╝                      \s
+                ██████╔╝╚██████╔╝╚██████╔╝██║  ██╗███████║   ██║                       \s
+                ╚═════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝   ╚═╝                       \s
+                Lev&Ross project                                                       \s
                 """+"\u001B[0m";
         System.out.println("\n"+start);
-        Scanner sc = new Scanner(System.in);
         sc.nextLine();
-
     }
 }
