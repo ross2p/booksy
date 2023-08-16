a5cd51e920a7bd0239d93b972c705a9ea50e5fac
Lev <levpredko@gmail.com>
2023-08-16 16:52:00 +0300

    fix

--- src/Salon/Class/Days.java
+++ src/Salon/Class/Days.java
@@ -1,36 +1,3 @@
 package Salon.Class;
 
-import java.time.DayOfWeek;
-
-public enum Days {
-    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
-
-    public static Days getDay(String s) {
-        try {
-            return Days.valueOf(s.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            return Days.MONDAY;
-        }
-    }
-
-    static public DayOfWeek getDayOfWeekFromDay(Days day) {
-        switch (day) {
-            case MONDAY:
-                return DayOfWeek.MONDAY;
-            case TUESDAY:
-                return DayOfWeek.TUESDAY;
-            case WEDNESDAY:
-                return DayOfWeek.WEDNESDAY;
-            case THURSDAY:
-                return DayOfWeek.THURSDAY;
-            case FRIDAY:
-                return DayOfWeek.FRIDAY;
-            case SATURDAY:
-                return DayOfWeek.SATURDAY;
-            case SUNDAY:
-                return DayOfWeek.SUNDAY;
-            default:
-                throw new IllegalArgumentException("Невідомий день: " + day);
-        }
-    }
-}
+public enum Days {}
